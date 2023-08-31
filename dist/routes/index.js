7f936f81a48ab6f322eb12f86e3d3d51dac2be0b
jonne <jonnenc@gmail.com>
2023-08-31 13:32:41 -0500

    delete type.d.ts and replace with types.ts. add route for specific user

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -5,5 +5,10 @@ var router = (0, express_1.Router)();
 router.get('/', function (req, res) {
     res.render('student-view.ejs');
 });
+router.get('/user/:id', function (req, res) {
+    var id = req.params.id;
+    // do all the databasing and fetch all information related to this id
+    // then render student-view given information
+});
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -4,6 +4,12 @@ const router: Router = Router();
 
 router.get('/', (req, res) => {
     res.render('student-view.ejs');
+});
+
+router.get('/user/:id', (req, res) => {
+    const { id } = req.params;
+    // do all the databasing and fetch all information related to this id
+    // then render student-view given information
 })
 
 export default router;
