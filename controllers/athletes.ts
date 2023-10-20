483f8135f5fb57f63a4f817d411733c54d1696cf
Reversal9 <jonnenc@gmail.com>
2023-10-20 13:24:39 -0500

    add get athlete by id and relevant statistics

--- controllers/athletes.ts
+++ controllers/athletes.ts
@@ -2,12 +2,24 @@ import { Response, Request } from "express";
 import { IAthlete } from "../types/athlete";
 import Athlete from "../models/athlete";
 import mongoose from "mongoose";
+import {IStatistic} from "../types/statistic";
+import Statistic from "../models/statistic";
 
 export const getAthletes = async(req: Request, res: Response) => {
     try {
         const athletes: IAthlete[] = await Athlete.find();
         console.log(athletes);
         res.status(200).json({ athletes });
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+export const getAthleteById = async(req: Request, res: Response, id: string) => {
+    try {
+        const athlete: IAthlete | null = await Athlete.findById({_id: id});
+        const statistics: IStatistic[] | null = await Statistic.find({athlete_id: new mongoose.Types.ObjectId(id)});
+        res.status(200).json({ "athlete": athlete, "statistics": statistics });
     } catch(err) {
         console.error(err);
     }
--- routes/athleteRoutes.ts
+++ routes/athleteRoutes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getAthletes } from "../controllers/athletes";
+import {getAthleteById, getAthletes} from "../controllers/athletes";
 
 const router: Router = Router();
 
 router.get('/athletes', getAthletes);
+router.get('/athletes/:id', (req, res) => {
+    getAthleteById(req, res, req.params.id);
+});
 
 export default router;
