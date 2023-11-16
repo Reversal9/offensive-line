f8af78ef245e2fae6370dade7dccc1a1e82bcc79
Reversal9 <jonnenc@gmail.com>
2023-11-16 13:25:59 -0600

    add get one statistic and update statistic and add statistic

--- controllers/statistics.ts
+++ controllers/statistics.ts
@@ -11,3 +11,83 @@ export const getStatistics = async(req: Request, res: Response) => {
         console.error(err);
     }
 };
+
+export const getStatistic = async(req: Request, res: Response) => {
+    try {
+        const statistic: IStatistic[] | null = await Statistic.findOne(
+            {_id: req.params.id }
+        );
+        res.status(200).json({ statistic });
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+export const addStatistic = async(req: Request, res: Response): Promise<void> => {
+    try {
+        const body = req.body;
+        const statistic: IStatistic = new Statistic({
+            month: body?.month,
+            year: body?.year,
+            position: body?.position,
+            grade: body?.grade,
+            height: body?.height,
+            weight: body?.weight,
+            squat: body?.squat,
+            bench: body?.bench,
+            clean: body?.clean,
+            bench_squat: body?.bench_squat,
+            total: body?.total,
+            jump: body?.jump,
+            reach: body?.reach,
+            vertical: body?.vertical,
+            forty_a: body?.forty_a,
+            forty_b: body?.forty_b,
+            forty_average: body?.forty_average,
+            laser_forty: body?.laser_forty,
+            pro_a: body?.pro_a,
+            pro_b: body?.pro_b,
+            pro_average: body?.pro_average,
+            group: body?.group,
+            zero_to_ten: body?.zero_to_ten,
+            ten_to_twenty: body?.ten_to_twenty,
+            twenty_to_thirty: body?.twenty_to_thirty,
+            thirty_to_forty: body?.thirty_to_forty,
+            elite: body?.elite,
+            player_height_score: body?.player_height_score,
+            active: body?.active,
+            athlete_id: body.athlete_id
+        });
+        const newStatistic: IStatistic = await statistic.save();
+        const allStatistics: IStatistic[] = await Statistic.find();
+        res.status(201).json({
+            message: "Member added",
+            statistic: newStatistic,
+            statistics: allStatistics
+        });
+    } catch(err) {
+        throw err;
+    }
+};
+
+export const updateStatistic = async(req: Request, res: Response): Promise<void> => {
+    try {
+        const {
+            params: { id },
+            body
+        } = req;
+        const updatedStatistic: IStatistic | null = await Statistic.findByIdAndUpdate(
+            { _id: id },
+            body,
+            { new: true }
+        );
+        const allStatistics: IStatistic[] = await Statistic.find();
+        res.status(200).json({
+            message: "Member updated",
+            statistic: updatedStatistic,
+            statistics: allStatistics
+        });
+    } catch(err) {
+        throw err;
+    }
+};
--- routes/statisticRoutes.ts
+++ routes/statisticRoutes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { getStatistics } from "../controllers/statistics";
+import {addStatistic, getStatistic, getStatistics, updateStatistic} from "../controllers/statistics";
 
 const router: Router = Router();
 
 router.get('/statistics', getStatistics);
+
+router.get('/statistics/:id', getStatistic);
+
+router.post('/statistics', addStatistic);
+
+router.put('/statistics/:id', updateStatistic);
+
 
 export default router;
