72cd2f4ccb8b789f03806053a170e9a08a7fcf55
Jonathan Bennikutty <73003837+GitHub-Jcb@users.noreply.github.com>
2024-02-05 13:34:17 -0600

    Updated index to include forms route

--- routes/index.ts
+++ routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import athleteRoutes from "./athleteRoutes";
 import statisticRoutes from "./statisticRoutes";
 import coachRoutes from "./coachRoutes";
+import formRoutes from "./formRoutes";
 
 const router: Router = Router();
 
 router.use(athleteRoutes);
 router.use(statisticRoutes);
 router.use(coachRoutes);
+router.use(formRoutes);
 
 export default router;
