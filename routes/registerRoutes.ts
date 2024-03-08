46c2197ec4bed45685037ede8045ca92b33aac50
Jonathan Bennikutty <73003837+GitHub-Jcb@users.noreply.github.com>
2024-03-08 13:01:36 -0600

    Update registerRoutes.ts

--- routes/registerRoutes.ts
+++ routes/registerRoutes.ts
@@ -1,14 +1,46 @@
 import express, { Router } from "express";
 import {addUser} from "../controllers/user";
 
+const bcrypt = require('bcrypt');
 
+type User = {
+    id: string,
+    username: any,
+    password: any
+}
+
+var users: User[] =  []
 
 const router: Router = Router();
+
+const passport = require('passport')
+const initializePassport = require('./passport-config')
+ initializePassport(
+  passport,
+  (username: string) =>  users.find((user: User) => user.username === username),
+ 
+)
+
 
 router.get('/register', (req, res) => {
     res.render('register.ejs');
 });
 
-router.post('/register',addUser )
+router.post('/register', (req, res) =>
+{
+    try{
+        const hashedPassword = bcrypt.hash(req.body.password,10)
+        users.push({
+            id: Date.now().toString(),
+            username: req.body.username,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch{
+        res.redirect('/register')
+    }
+
+    console.log(users)
+})
 
 export default router;
