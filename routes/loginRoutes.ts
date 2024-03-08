b265202876807726a123879d38d6eca5c0a5cb53
Jonathan Bennikutty <73003837+GitHub-Jcb@users.noreply.github.com>
2024-03-08 13:00:25 -0600

    Update loginRoutes.ts

--- routes/loginRoutes.ts
+++ routes/loginRoutes.ts
@@ -1,9 +1,28 @@
 import express, { Router } from "express";
+import {authenticateUser} from "../controllers/user";
+import passport from "passport";
+
+
+
+var cookieParser = require('cookie-parser')
+
+
 
 
 const router: Router = Router();
 
 router.get('/login', (req, res) => {
     res.render('login.ejs');
+    /*console.log('Cookies: ', req.cookies)
+    console.log('Signed Cookies: ', req.signedCookies)
+    */
 });
+
+router.post('/login', passport.authenticate('local',
+{
+    successRedirect: '/athletes',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
 export default router;
