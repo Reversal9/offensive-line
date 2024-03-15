6400167117a7b05de2e285d5f0964e27bf1d597d
Jonathan Bennikutty <73003837+GitHub-Jcb@users.noreply.github.com>
2024-03-15 14:30:18 -0500

    Update passport-config.ts

--- routes/passport-config.ts
+++ routes/passport-config.ts
@@ -1,42 +1,29 @@
-import { PassportStatic } from "passport"
-import { authenticateUser } from "../controllers/user"
-
-
-
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
-
-  async function initialize(passport: PassportStatic, getUserByUsername: any)
-{
-    const authenticateUser =  async  (username: string, password: string, done: any) => {
-         const user =  getUserByUsername(username)
-
-         console.log(user.password)
-         console.log(password)
-         console.log("deez nut")
-        if(user == null)
-        {
-            return done(null, false, {message: "No user with that username"})
-        }
-
-        try{
-            if(await bcrypt.compare(new String(password), new String(user.password))) // give up
-            {
-                return done(null, user)
-            } else{
-                return done(null,false, {message: "Password incorrect"})
-            }
-        }catch(e){
-            return done(e)
-        }
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email)
+    if (user == null) {
+      return done(null, false, { message: 'No user with that email' })
     }
-    passport.use(new LocalStrategy({usernameField: 'username'}, authenticateUser))
-    passport.serializeUser((user, done)=> {})
-    passport.deserializeUser((id, done)=> {})
 
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'Password incorrect' })
+      }
+    } catch (e) {
+      return done(e)
+    }
+  }
 
-
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
 }
 
 module.exports = initialize
