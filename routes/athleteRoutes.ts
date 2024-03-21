4928f4f7074911b0e7a3de18ece3f2d2e50d9859
Jonathan Bennikutty <73003837+GitHub-Jcb@users.noreply.github.com>
2024-03-21 13:01:05 -0500

    Update athleteRoutes.ts

--- routes/athleteRoutes.ts
+++ routes/athleteRoutes.ts
@@ -1,11 +1,12 @@
-import express, { Router } from "express";
+/*import express, { Router } from "express";
 import {getAthleteById, getAthletes} from "../controllers/athletes";
 
 const router: Router = Router();
 
-router.get('/athletes', getAthletes);
-router.get('/athletes/:id', (req, res) => {
+router.get('/athletes',  getAthletes);
+router.get('/athletes/:id',  (req, res) => {
     getAthleteById(req, res, req.params.id);
 });
 
 export default router;
+*/
