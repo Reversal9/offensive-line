d26444ca261e97ad38c0edcd7e03c68d1ee69a18
Jonathan Bennikutty <73003837+GitHub-Jcb@users.noreply.github.com>
2024-03-21 13:02:16 -0500

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -1,14 +1,23 @@
+
 import { Router } from "express";
-import athleteRoutes from "./athleteRoutes";
-import statisticRoutes from "./statisticRoutes";
-import coachRoutes from "./coachRoutes";
-import formRoutes from "./formRoutes";
+//import athleteRoutes from "./athleteRoutes";
+//import statisticRoutes from "./statisticRoutes";
+//import coachRoutes from "./coachRoutes";
+//import formRoutes from "./formRoutes";
+//import loginRoutes from "./loginRoutes";
+//import registerRoutes from "./registerRoutes";
+
+//const router: Router = Router();
+
+//router.use(athleteRoutes);
+//router.use(statisticRoutes);
+//router.use(coachRoutes);
+//router.use(formRoutes);
+//router.use(loginRoutes);
+//router.use(registerRoutes);
+
 
 const router: Router = Router();
 
-router.use(athleteRoutes);
-router.use(statisticRoutes);
-router.use(coachRoutes);
-router.use(formRoutes);
-
 export default router;
+
